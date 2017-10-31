1a9d6c49bf41879a5091255a2bddbb5b3b73f76f
Hake Huang <hake.huang@nxp.com>
2017-10-31 17:37:48 +0800

    add void test framework
    Signed-off-by: Hake Huang <hake.huang@nxp.com>

---
+++ test/test_all.rb
@@ -0,0 +1,36 @@
+#! ruby -I../
+
+require_relative '../lib/ebngen'
+require 'pathname'
+require 'minitest/autorun'
+require 'yaml'
+#require 'yml_merger'
+
+
+class MergeTest < Minitest::Test
+
+  #def initialize(option)
+  #	@NAME = "merge"
+
+ # end
+  def test_mebed
+  end
+
+  def test_records
+  end
+
+  def test_xml
+  end
+
+  def test_unifmt
+
+  end
+
+  def test_unifmt_cmake
+  end
+
+  def test_unifmt_mdk
+
+  end
+
+end
